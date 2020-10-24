86d691f6c91bb4be499fe757c3a383b538a5bae0
湯浅 信彦 <yuasys@gmail.com>
2020-10-24 11:31:55 +0900

    Upd:index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var http = require('http');
+const http = require('http');
 http.createServer(function (request, response) {
+  const contents = `<h1>Helo World</h1>
+  <p>いじってみました</p>`
   response.writeHead(200, {
-    'Content-Type': 'text/plain'
+    'Content-Type': 'text/html'
   });
-  response.end('Hello Hello\n いじってみたよ');
+
+  response.end(contents);
 }).listen(3000);
 
 console.log('Server running at http://127.0.0.1:3000');
